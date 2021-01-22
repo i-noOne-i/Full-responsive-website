ffb6d5e9eb8c22ea817acc867c30c80ae0284f74
i-noOne-i <pijushdebnath3@gmail.com>
2021-01-22 21:51:51 +0530

    commit

--- Ptoject-6/app.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector("#mobileMenu");
-const menuLinks = document.querySelector(".navbarMenu");
-
-menu.addEventListener("click", function () {
-  menu.classList.toggle("is-active");
-  menuLinks.classList.toggle("active");
-});
