ce4138be234a2e84964599dd2d0b01ca13019db9
i-noOne-i <pijushdebnath3@gmail.com>
2021-01-22 21:51:22 +0530

    messge

--- Ptoject-6/app.js
+++ Ptoject-6/app.js
@@ -1,40 +1,7 @@
-function calculateAge() {
-  let birthDate = new Date(document.getElementById("birthDate").value);
-  let birthDateDay = birthDate.getDate();
-  let birthDateMonth = birthDate.getMonth();
-  let birthDateYear = birthDate.getFullYear();
+const menu = document.querySelector("#mobileMenu");
+const menuLinks = document.querySelector(".navbarMenu");
 
-  let todayDate = new Date();
-  let todayDay = todayDate.getDate();
-  let todayMonth = todayDate.getMonth();
-  let todayYear = todayDate.getFullYear();
-
-  let calculatedAge = 0;
-
-  calculatedAge = todayYear - birthDateYear;
-
-  if (todayMonth > birthDateMonth) {
-    calculatedAge = todayYear - birthDateYear;
-  } else if (todayMonth == birthDateMonth) {
-    if (todayDay >= birthDateDay) {
-      calculatedAge = todayYear - birthDateYear;
-    } else {
-      calculatedAge = todayYear - birthDateYear - 1;
-    }
-  } else {
-    calculatedAge = todayYear - birthDateYear - 1;
-  }
-
-  let outputValue = calculatedAge;
-  document.getElementById("calculatedAge").innerHTML =
-    calculatedAge +
-    " Years" +
-    " " +
-    Math.abs(calculatedMonth) +
-    " " +
-    "Months" +
-    " " +
-    Math.abs(calculatedDay) +
-    " " +
-    "Days";
-}
+menu.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+});
